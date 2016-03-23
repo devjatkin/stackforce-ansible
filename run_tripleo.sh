e365d0b7aeaa0db178c88bcb579ce6121f06e97c
Kirill Mashchenko <lastkrick@gmail.com>
2016-03-23 13:53:18 +0300

    Steps fix

--- run_tripleo.sh
+++ run_tripleo.sh
@@ -11,6 +11,6 @@ pip install -r requirements.txt
 export LOCAL_TMP_DIR=${LOCAL_TMP_DIR:=/tmp}
 export ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook -i "localhost ansible_python_interpreter=python," --extra-vars username=$STACKFORCE_USERNAME -c local test/playbooks/up.yml
-LANG=C ssh -t centos@$(openstack --os-cloud tripleo server show controller01 -c addresses -f value | cut -d ' ' -f 2) "ansible-playbook -i stackforce-ansible/inventory/dynlxc.py --sudo stackforce-ansible/playbooks/stackforce.yml"
+LANG=C ansible-playbook -i /tmp/inventory --extra-vars 'username=centos inventory=/tmp/inventory containers=/tmp/containers.yml' test/playbooks/controller.yml
 LANG=C ssh -t centos@$(openstack --os-cloud tripleo server show controller01 -c addresses -f value | cut -d ' ' -f 2) "ansible-playbook -i stackforce-ansible/inventory/dynlxc.py --sudo stackforce-ansible/playbooks/create_lxc_containers.yml"
 LANG=C ssh -t centos@$(openstack --os-cloud tripleo server show controller01 -c addresses -f value | cut -d ' ' -f 2) "ansible-playbook -i stackforce-ansible/inventory/dynlxc.py --sudo stackforce-ansible/playbooks/stackforce.yml"
